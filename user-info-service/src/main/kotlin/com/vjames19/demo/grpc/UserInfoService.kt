598334e0dcbdc6be037107c0fe27b2319e0f66b2
Victor Reventos <victor.reventos1@gmail.com>
2016-12-10 13:29:12 -0600

    user info service

--- user-info-service/src/main/kotlin/com/vjames19/demo/grpc/UserInfoService.kt
+++ user-info-service/src/main/kotlin/com/vjames19/demo/grpc/UserInfoService.kt
@@ -1,7 +1,44 @@
 package com.vjames19.demo.grpc
 
+import com.vjames19.demo.grpc.proto.UserInfo
+import com.vjames19.demo.grpc.proto.UserInfoServiceGrpc.UserInfoServiceImplBase
+import com.vjames19.demo.grpc.proto.UserRequest
+import io.grpc.Status
+import io.grpc.stub.StreamObserver
+
 /**
  * Created by vreventos on 12/10/16.
  */
-class UserInfoService {
+class UserInfoService : UserInfoServiceImplBase() {
+
+    override fun getUser(request: UserRequest, responseObserver: StreamObserver<UserInfo>) {
+        if (!users.contains(request.id)) {
+            responseObserver.onError(Status.NOT_FOUND.asException())
+        } else {
+            responseObserver.onNext(users[request.id])
+            responseObserver.onCompleted()
+        }
+    }
+
+    companion object {
+        val users: Map<Long, UserInfo> = users()
+
+        private fun users(): Map<Long, UserInfo> {
+            val names = listOf("Victor", "John", "Juan", "Pedor", "Jess", "Ly")
+            val pairs = names.mapIndexed { i, name ->
+                i.toLong() to user(i.toLong(), name)
+            }.toTypedArray()
+
+            return hashMapOf(*pairs)
+        }
+
+        private fun user(userId: Long, userName: String): UserInfo {
+            return UserInfo.newBuilder().apply {
+                id = userId
+                name = userName
+                email = "${userName}@test.com"
+                phone = "787-123-1234"
+            }.build()
+        }
+    }
 }
